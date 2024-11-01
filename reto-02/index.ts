d03a2341b17f2dc582b1db4054489678c3718250
efe <eefee1987@gmail.com>
2024-11-01 13:20:49 +0100

    add challenge 02

--- reto-02/index.ts
+++ reto-02/index.ts
@@ -1,5 +1,20 @@
+const EMPATE = 0;
+
 function battleHorde(zombies: string, humans: string): string {
-  return 'x';
+  if (zombies.length !== humans.length) {
+    throw new Error('Las cadenas deben tener la misma longitud');
+  }
+
+  let difference = 0;
+
+  for (let i = 0; i < zombies.length; i++) {
+    difference += parseInt(zombies[i]) - parseInt(humans[i]);
+  }
+
+  if (difference === EMPATE) return 'x';
+  return difference > EMPATE ? `${difference}z` : `${-difference}h`;
 }
 
+// Pruebas
 console.log(battleHorde('242', '334')); // -> "2h"
+console.log(battleHorde('4598', '7682')); // -> "3z"
