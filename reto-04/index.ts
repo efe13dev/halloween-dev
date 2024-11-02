bc31c4a721497bb02afd3a7a0b97838cb21a2c32
efe <eefee1987@gmail.com>
2024-11-02 13:13:03 +0100

    create challenge 04

---
+++ reto-04/index.ts
@@ -0,0 +1,13 @@
+function findTheKiller(whisper: string, suspects: string[]): string {
+  return '';
+}
+
+const whisper = 'd~~~~~a';
+const suspects = [
+  'Dracula',
+  'Freddy Krueger',
+  'Jason Voorhees',
+  'Michael Myers'
+];
+
+console.log(findTheKiller(whisper, suspects));
