4e0b43bdb05438d7a81c3b1c1c4afd7a67cf0472
efe <eefee1987@gmail.com>
2024-11-02 18:25:35 +0100

    add challenge 04

--- reto-04/index.ts
+++ reto-04/index.ts
@@ -1,5 +1,30 @@
 function findTheKiller(whisper: string, suspects: string[]): string {
-  return '';
+  const whisperLower = whisper.toLowerCase();
+  const DOLLAR_SIGN_LENGTH = 1;
+  const isComplete = whisperLower.endsWith('$');
+  const effectiveLength = isComplete
+    ? whisperLower.length - DOLLAR_SIGN_LENGTH
+    : whisperLower.length;
+
+  return suspects
+    .filter((suspect) => {
+      const suspectLower = suspect.toLowerCase();
+
+      if (isComplete && effectiveLength !== suspectLower.length) return false;
+      if (!isComplete && effectiveLength > suspectLower.length) return false;
+
+      const START_INDEX = 0;
+      const whisperSlice = whisperLower.slice(START_INDEX, effectiveLength);
+
+      for (const [i, currentChar] of [...whisperSlice].entries()) {
+        if (currentChar !== '~' && currentChar !== suspectLower[i]) {
+          return false;
+        }
+      }
+
+      return true;
+    })
+    .join(',');
 }
 
 const whisper = 'd~~~~~a';
